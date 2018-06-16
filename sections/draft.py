8a85fdf534bee645ab97d79e438f83daad846565
Hervé Beraud <herveberaud.pro@gmail.com>
2018-06-17 01:04:42 +0200

    Create draft.py

---
+++ sections/draft.py
@@ -0,0 +1,6 @@
+https://fr.wikipedia.org/wiki/Th%C3%A9orie_de_l%27information
+https://fr.wikipedia.org/wiki/Lambda-calcul
+https://fr.wikipedia.org/wiki/Paradoxe_de_l%27information
+https://fr.wikipedia.org/wiki/Bay%C3%A9sianisme
+https://fr.wikipedia.org/wiki/Gregor_Mendel
+https://fr.wikipedia.org/wiki/Ronald_Aylmer_Fisher
